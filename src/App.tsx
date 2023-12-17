ce9bfad59c1ed2013cc2976655fcabf429f601c6
jun <jun@Ubt2200.aaaaa>
2023-12-17 22:28:53 +0900

    ベースコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState }  from 'react';
 import './App.css';
 import * as THREE from 'three'
-import { Canvas, useFrame, ThreeElements  } from '@react-three/fiber'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Text } from '@react-three/drei';
 
 const Box = (props: ThreeElements['mesh']) => {
   const meshRef = useRef<THREE.Mesh>(null!)
@@ -22,6 +23,32 @@ const Box = (props: ThreeElements['mesh']) => {
   )
 }
 
+const Text1 = (props: { font?: string, fontSize?: number}) => {
+  const texsRef = useRef({} as any)
+  useFrame(() => {
+    texsRef.current.rotation.x += 0.01
+  });
+
+  return (
+    <group ref={texsRef} position={[-2, 1, 0]} >
+        <Text font={props.font} fontSize={props.fontSize} color={'#ff0203'}>HELLO</Text>
+    </group>
+  )
+}
+
+const Text2 = (props: { font?: string, fontSize?: number}) => {
+  const texsRef = useRef({} as any)
+  useFrame(() => {
+    texsRef.current.rotation.x += 0.01
+  });
+
+  return (
+    <group ref={texsRef} position={[1, 0, 2]} >
+        <Text font={props.font} fontSize={props.fontSize} color={'#03ffff'}>WORLD</Text>
+    </group>
+  )
+}
+
 const App = () => {
   return (
     <div id="canvas-container">
@@ -30,6 +57,8 @@ const App = () => {
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
+        <Text1 font="/Roboto-Black.ttf" fontSize={1} />
+        <Text2 font="/Roboto-Black.ttf" fontSize={1}/>
       </Canvas>
     </div>
   );
