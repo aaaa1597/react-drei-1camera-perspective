749226e905309c1dcc2dee3852796b8844f59854
aaaa1597 <tokushi2011@gmail.com>
2024-01-05 19:55:46 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useRef} from 'react';
 import './App.css';
 import { Canvas, useFrame, MeshProps, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
-import { OrbitControls, Environment } from '@react-three/drei'
+import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei'
 
 const Box = (props: MeshProps) => {
   const ref = useRef<THREE.Mesh>(null!)
@@ -21,15 +21,25 @@ const Box = (props: MeshProps) => {
   )
 }
 
+const Rig = () => {
+  const {camera} = useThree()
+  return useFrame(() => {
+    camera.position.lerp(new THREE.Vector3(0, 3, 10), 0.05)
+    camera.lookAt(0, 0, 0)
+  })
+}
+
 const App = () => {
   return (
     <div style={{ width: "100vw", height: "75vh" }}>
       <Canvas camera={{ position: [0, 3, 5] }}>
+        <PerspectiveCamera makeDefault position={[0, -100, 10]}/>
         <Box position={[1, 1, 1]} name="A" />
         <Environment preset="forest" background />
         <OrbitControls />
         <axesHelper args={[5]} />
         <gridHelper />
+        <Rig />
       </Canvas>
     </div>
   );
